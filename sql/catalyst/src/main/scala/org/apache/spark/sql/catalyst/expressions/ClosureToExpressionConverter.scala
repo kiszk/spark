533af065a1c0792818e09a5088d0321cf6a488da
Josh Rosen <joshrosen@databricks.com>
2016-03-27 16:25:12 -0700

    Stack walking

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/ClosureToExpressionConverter.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/ClosureToExpressionConverter.scala
@@ -18,8 +18,9 @@
 package org.apache.spark.sql.catalyst.expressions
 
 
+import java.util.concurrent.atomic.AtomicInteger
 import javassist.{CtMethod, CtMember, ClassPool}
-import javassist.bytecode.{Opcode, InstructionPrinter}
+import javassist.bytecode.{ConstPool, Opcode, InstructionPrinter}
 import javassist.bytecode.analysis.Analyzer
 
 object ClosureToExpressionConverter {
@@ -27,28 +28,59 @@ object ClosureToExpressionConverter {
   val analyzer = new Analyzer()
   val classPool = ClassPool.getDefault
 
+  var nextIdentifier = new AtomicInteger(-1)
+
   def analyzeMethod(method : CtMethod): Unit = {
     println ("-" * 80)
     println (method.getName)
     println ("-" * 80)
+
     val frames = analyzer.analyze(method)
     val instructions = method.getMethodInfo().getCodeAttribute.iterator()
     val cp = method.getMethodInfo.getConstPool
+    var stackHeight = 0
     while (instructions.hasNext) {
+      print(s"stack height is $stackHeight | ")
       val pos = instructions.next()
       val op = instructions.byteAt(pos)
-      if (op == Opcode.INVOKEVIRTUAL) {
-        println(frames(pos))
-        val mrClassName = cp.getMethodrefClassName(instructions.u16bitAt(pos + 1))
-        val mrMethName = cp.getMethodrefName(instructions.u16bitAt(pos + 1))
-        val mrDesc = cp.getMethodrefType(instructions.u16bitAt(pos + 1))
-        val ctClass = classPool.get(mrClassName)
-        val ctMethod = ctClass.getMethod(mrMethName, mrDesc)
-        analyzeMethod(ctMethod)
-        println(InstructionPrinter.instructionString(instructions, pos, cp))
-      } else {
-        println(InstructionPrinter.instructionString(instructions, pos, cp))
+      stackHeight += Opcode.STACK_GROW(op)
+      val mnemonic = InstructionPrinter.instructionString(instructions, pos, cp)
+      op match {
+        case Opcode.ALOAD_0 =>
+          println(s"stack$stackHeight = local0")
+        case Opcode.ILOAD_1 =>
+          println(s"stack$stackHeight = local1")
+        case Opcode.ICONST_1 =>
+          println(s"stack$stackHeight = 1")
+        case Opcode.IADD =>
+          println(s"stack$stackHeight = stack${stackHeight - 1} + stack${stackHeight}")
+        case Opcode.LDC =>
+          val cp_index = instructions.byteAt(pos + 1)
+          val value = cp.getTag(cp_index) match {
+            case ConstPool.CONST_Integer => cp.getIntegerInfo(cp_index)
+          }
+          println(s"stack$stackHeight = $value")
+        case Opcode.INVOKEVIRTUAL =>
+          val mrClassName = cp.getMethodrefClassName(instructions.u16bitAt(pos + 1))
+          val mrMethName = cp.getMethodrefName(instructions.u16bitAt(pos + 1))
+          val mrDesc = cp.getMethodrefType(instructions.u16bitAt(pos + 1))
+          val ctClass = classPool.get(mrClassName)
+          val ctMethod = ctClass.getMethod(mrMethName, mrDesc)
+          val numParameters = ctMethod.getParameterTypes.length
+          println(s"stack$stackHeight = ($mrClassName stack${stackHeight - numParameters}).$mrMethName(${(1 to numParameters).map(i => s"stack${stackHeight - i}").mkString(", ")})")
+          analyzeMethod(ctMethod)
+          stackHeight -= numParameters + 1
+        case Opcode.IRETURN =>
+          println(s"Return stack${stackHeight + 1}")
+        case _ =>
+          println(s"Unknown opcode $mnemonic")
       }
+//      if (op == Opcode.INVOKEVIRTUAL) {
+
+//        println(InstructionPrinter.instructionString(instructions, pos, cp))
+//      } else {
+//        println(InstructionPrinter.instructionString(instructions, pos, cp))
+//      }
     }
     println
   }
