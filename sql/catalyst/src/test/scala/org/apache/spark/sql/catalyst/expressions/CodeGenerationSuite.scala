687a522c8d0b2cfc41a2932fd972cd108243e7ca
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2016-06-22 21:41:01 +0900

    addressed review comments

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CodeGenerationSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CodeGenerationSuite.scala
@@ -96,7 +96,7 @@ class CodeGenerationSuite extends SparkFunSuite with ExpressionEvalHelper {
     val expressions = Seq(CreateArray(List.fill(length)(EqualTo(Literal(1), Literal(1)))))
     val plan = GenerateMutableProjection.generate(expressions)
     val actual = plan(new GenericMutableRow(length)).toSeq(expressions.map(_.dataType))
-    val expected = Seq(GenericArrayData.allocate(Seq.fill(length)(true)))
+    val expected = Seq(GenericArrayData.allocate(Array.fill(length)(true)))
 
     if (!checkResult(actual, expected)) {
       fail(s"Incorrect Evaluation: expressions: $expressions, actual: $actual, expected: $expected")
@@ -113,7 +113,7 @@ class CodeGenerationSuite extends SparkFunSuite with ExpressionEvalHelper {
     val actual = plan(new GenericMutableRow(length)).toSeq(expressions.map(_.dataType))
     val expected = Seq(new ArrayBasedMapData(
       GenericArrayData.allocate(0 until length),
-      GenericArrayData.allocate(Seq.fill(length)(true))))
+      GenericArrayData.allocate(Array.fill(length)(true))))
 
     if (!checkResult(actual, expected)) {
       fail(s"Incorrect Evaluation: expressions: $expressions, actual: $actual, expected: $expected")
